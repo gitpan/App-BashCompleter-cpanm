fa6bd3f82e9b74c564fda8d28af6bf30c9e95394
perlancar <perlancar@gmail.com>
2014-11-09 18:29:23 +0000

    Import of PERLANCAR/App-BashCompleter-cpanm-0.01.tar.gz from CPAN.
    gitpan-cpan-distribution: App-BashCompleter-cpanm
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         PERLANCAR/App-BashCompleter-cpanm-0.01.tar.gz
    gitpan-cpan-author:       PERLANCAR
    gitpan-cpan-maturity:     released

---
+++ lib/App/BashCompleter/cpanm.pm
@@ -0,0 +1,54 @@
+package App::BashCompleter::cpanm;
+
+our $DATE = '2014-11-10'; # DATE
+our $VERSION = '0.01'; # VERSION
+
+1;
+# ABSTRACT: Bash completion for cpanm
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+App::BashCompleter::cpanm - Bash completion for cpanm
+
+=head1 VERSION
+
+This document describes version 0.01 of App::BashCompleter::cpanm (from Perl distribution App-BashCompleter-cpanm), released on 2014-11-10.
+
+=head1 SYNOPSIS
+
+See L<_cpanm> included in this distribution.
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/App-BashCompleter-cpanm>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/perlancar/perl-App-BashCompleter-cpanm>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=App-BashCompleter-cpanm>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
+
+=head1 AUTHOR
+
+perlancar <perlancar@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by perlancar@cpan.org.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
